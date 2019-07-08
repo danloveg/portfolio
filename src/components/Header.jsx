293a506254508b3169e72f951b54337e2265c443
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-08 15:22:32 -0500

    Fix logo not showing up in Github Pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import "./Header.css"
+import logo from "../assets/logo.png"
 
 function Header() {
     return (
         <div className="fixed-header">
             <div className="container">
-                <img className="header-item header-image" alt="Square Logo" src="/logo.png"></img>
+                <img className="header-item header-image" alt="Square logo" src={logo}></img>
                 <h1 className="header-item">Daniel Lovegrove</h1>
             </div>
         </div>
