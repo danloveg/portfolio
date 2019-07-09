fcaa516e442f31065d57a2ce475688cbdb18fbd4
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-09 15:03:59 -0400

    Updates

---
+++ precache-manifest.9e72cada807a7c56ee7f7b491f8e42b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3127e644e349ff171345c29a54c48b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7bf689084fa97a920fa",
+    "url": "/portfolio/static/css/main.326c64d9.chunk.css"
+  },
+  {
+    "revision": "fb298cbcb64ad6420cff",
+    "url": "/portfolio/static/js/2.82eaf29c.chunk.js"
+  },
+  {
+    "revision": "d7bf689084fa97a920fa",
+    "url": "/portfolio/static/js/main.2af97cd0.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "7a246d884a40c30154f3e4b13832b26d",
+    "url": "/portfolio/static/media/myself.7a246d88.png"
+  },
+  {
+    "revision": "be17a7b5ebd8d3c867f612f811cf7b4e",
+    "url": "/portfolio/static/media/myself_small.be17a7b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2310dfbf6591331c38c6bfce98e892c9.js"
+  "/portfolio/precache-manifest.9e72cada807a7c56ee7f7b491f8e42b8.js"
 );
 
 self.addEventListener('message', (event) => {
