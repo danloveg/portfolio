a7052df9be7229afb514deb2d94c9c30258e452b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-09 12:28:29 -0400

    Add links in header to LinkedIn and GitHub

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import "./Header.css"
 import logo from "../assets/logo.png"
+import linkedin from "../assets/linkedin.png"
+import github from "../assets/github.png"
 
 function Header() {
     return (
@@ -8,6 +10,22 @@ function Header() {
             <div className="container">
                 <img className="header-item header-image" alt="Square logo" src={logo}></img>
                 <h1 className="header-item">Daniel Lovegrove</h1>
+                <table className="header-links-item">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="https://linkedin.com/in/daniel-lovegrove">
+                                    <img alt="LinkedIn Logo" src={linkedin}></img>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://github.com/danloveg">
+                                    <img alt="GitHub Logo" src={github}></img>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
