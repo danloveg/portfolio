c4256f6cd046ab7bfb1da6f2e7fa697b64d120d9
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-16 11:36:06 -0400

    Add project images to Projects page

--- src/projects/ProjectItem.jsx
+++ src/projects/ProjectItem.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from "react"
+import Media from "react-media"
 
 class ProjectItem extends Component {
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
+
+                <Media query="(max-width: 800px)">
+                    {matches => matches ?
+                        (<img src={this.props.smallImage} alt={this.props.title}></img>) :
+                        (<img src={this.props.bigImage} alt={this.props.title}></img>)
+                    }
+                </Media>
+
+                <br />
                 <CodeLink codeLink={this.props.codeLink} />
                 <br />
                 <a href={this.props.demoLink}>Demo</a>
--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -3,24 +3,41 @@ import "./Projects.css"
 
 import ProjectItem from "./ProjectItem"
 
+import umIrisImage from "../assets/projects/um_iris_project.png"
+import umIrisImageSmall from "../assets/projects/um_iris_project_sm.png"
+import breadtunesImage from "../assets/projects/breadtunes_project.png"
+import breadtunesImageSmall from "../assets/projects/breadtunes_project_sm.png"
+import portfolioImage from "../assets/projects/portfolio_project.png"
+import portfolioImageSmall from "../assets/projects/portfolio_project_sm.png"
+import cityOfWinnipegImage from "../assets/projects/city_of_winnipeg_project.png"
+import cityOfWinnipegImageSmall from "../assets/projects/city_of_winnipeg_project_sm.png"
+
 class Projects extends Component {
     render() {
         return (
             <div className="projects-container">
                 <ProjectItem
                     title={"UM Iris"}
+                    bigImage={umIrisImage}
+                    smallImage={umIrisImageSmall}
                     codeLink={"https://github.com/danloveg/Human-Computer-Interactions-Final-Project"}
                     demoLink={"https://danloveg.github.io/Human-Computer-Interactions-Final-Project"} />
                 <ProjectItem
                     title={"BreadTunes Music Player"}
+                    bigImage={breadtunesImage}
+                    smallImage={breadtunesImageSmall}
                     codeLink={"https://github.com/danloveg/Software-Engineering-Project"}
                     demoLink={"https://danloveg.github.io"} />
                 <ProjectItem
                     title={"Portfolio Website"}
+                    bigImage={portfolioImage}
+                    smallImage={portfolioImageSmall}
                     codeLink={"https://github.com/danloveg/portfolio"}
                     demoLink={"http://dlovegrove.ca"} />
                 <ProjectItem
                     title={"City of Winnipeg MyUtilityBill"}
+                    bigImage={cityOfWinnipegImage}
+                    smallImage={cityOfWinnipegImageSmall}
                     codeLink={undefined}
                     demoLink={"https://myutilitybill.winnipeg.ca/UtilityPortal/signin"} />
             </div>
