1222724d2baae7b233b4b413190d853f95b71aa4
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-17 15:31:28 -0400

    Add picture and basic styling to About section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,11 +1,28 @@
 import React, {Component} from "react"
 
+import profilePic from "../assets/about_me_picture.png"
+import "./About.css"
+
 class About extends Component {
     render() {
         return (
-            <div className="section-title">
+            <div>
                 <br />
-                about me
+                <div className="section-title">
+                    about me
+                </div>
+                <div className="about-row">
+                    <div className="about-column">
+                        <div className="column-item-container">
+                            <img className="profile-pic" src={profilePic} alt="A portrait of myself"></img>
+                        </div>
+                    </div>
+                    <div className="about-column">
+                        <div className="about-text">
+                            Some stuff about me.
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
