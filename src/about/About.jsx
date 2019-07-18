bef2132f7c04f24a22cd99035b5e8bd2bff9f884
Daniel Lovegrove <d.lovegrove11@gmail.com>
2019-07-18 11:00:46 -0400

    Finish first version of About and Contact section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -13,13 +13,22 @@ class About extends Component {
                 </div>
                 <div className="about-row">
                     <div className="about-column">
-                        <div className="column-item-container">
+                        <div className="profile-pic-container">
                             <img className="profile-pic" src={profilePic} alt="A portrait of myself"></img>
                         </div>
                     </div>
                     <div className="about-column">
                         <div className="about-text">
-                            Some stuff about me.
+                            I am a computer engineering graduate from Manitoba, Canada
+                            <span role="img" aria-label="Maple Leaf">🍁</span>with a passion for all things
+                            programming. I have professional full stack development experience working on public
+                            applications for the City of Winnipeg. I am committed to writing clean production-grade code
+                            and I don't shy from writing awesome documentation and solid tests. My design philosophy is
+                            KISS (Keep it Simple, Stupid), and so my code and designs are typically minimal. 
+                            <br />
+                            <br />
+                            I am a minimalist at home, and spend lots of time on my bicycle, on the yoga mat, and
+                            experimenting in the kitchen. I am currently living in Ontario, Canada.
                         </div>
                     </div>
                 </div>
--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -4,9 +4,28 @@ import "./Contact.css"
 class Contact extends Component {
     render() {
         return (
-            <div className="section-title">
-                <br />
-                contact me
+            <div>
+                <div className="section-title">
+                    <br />
+                    contact me
+                </div>
+                <div className="contact-row">
+                    <div className="contact-column">
+                        <div className="contact-item">
+                            <a href="https://linkedin.com/in/daniel-lovegrove" className="contact-link">LinkedIn</a>
+                        </div>
+                    </div>
+                    <div className="contact-column">
+                        <div className="contact-item">
+                            <a href="mailto:dlovegrove12@gmail.com" className="contact-link">Email</a>
+                        </div>
+                    </div>
+                    <div className="contact-column">
+                        <div className="phone-number contact-item">
+                            +1 204 210 0568
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
